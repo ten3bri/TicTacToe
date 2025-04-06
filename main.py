25b8c59930d0c34ba1baa018097bd8346b076af7
tenebri <wojtek12900@gmail.com>
2025-04-06 14:58:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def draw_board(board):
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
