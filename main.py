03d32def8bf1aea6a694d484615af988de29b013
tenebri <wojtek12900@gmail.com>
2025-04-06 15:12:42 +0200

    Add draw_board and check_winner functions

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def draw_board(board):
+    for row in board:
+        print(' | ').join(row)
+        print('-' * 9)
+
+def check_winner(board):
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] != ' ':
+            return True
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return True
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return True
+
+    return False
 
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
